a892282a74717be56598b38d7ec6488b85be4e66
apugithub <sgupdate1988@gmail.com>
2019-01-21 22:44:11 +0530

    first.scala added

---
+++ first.scala
@@ -0,0 +1,12 @@
+//https://spark.apache.org/examples.html
+package pakg1
+
+object test {
+  def main (args : Array[String]){
+    val a = 3
+    val b = a+1
+    println (a)
+    //println("\n")
+    println(b)
+  }
+}
